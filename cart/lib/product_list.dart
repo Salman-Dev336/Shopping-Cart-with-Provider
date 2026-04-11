b10e904ca435317061a202896a5ccfb79350dd32
Salman Khan <salmantechhub336@gmail.com>
2026-04-12 00:54:27 +0500

    add price and units

--- cart/lib/product_list.dart
+++ cart/lib/product_list.dart
@@ -12,19 +12,26 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
-
-   List<String> productName = ['Mango' , 'Orange' , 'Grapes' , 'Banana' , 'Chery' , 'Peach','Mixed Fruit Basket',] ;
-  List<String> productUnit = ['KG' , 'Dozen' , 'KG' , 'Dozen' , 'KG' , 'KG','KG',] ;
-  List<int> productPrice = [10, 20 , 30 , 40 , 50, 60 , 70 ] ;
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Chery',
+    'Peach',
+    'Mixed Fruit Basket',
+  ];
+  List<String> productUnit = ['KG', 'Dozen', 'KG', 'Dozen', 'KG', 'KG', 'KG'];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
   List<String> productImage = [
-    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg' ,
-    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg' ,
-    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg' ,
-    'https://image.shutterstock.com/image-photo/banana-isolated-on-white-background-600w-610892247.jpg' ,
-    'https://image.shutterstock.com/image-photo/cherry-isolated-on-white-background-600w-610892248.jpg' ,
-    'https://image.shutterstock.com/image-photo/peach-isolated-on-white-background-600w-610892250.jpg' ,
-    'https://image.shutterstock.com/image-photo/mixed-fruit-basket-isolated-on-600w-610892251.jpg' ,
-  ] ;
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+    'https://image.shutterstock.com/image-photo/banana-isolated-on-white-background-600w-610892247.jpg',
+    'https://image.shutterstock.com/image-photo/cherry-isolated-on-white-background-600w-610892248.jpg',
+    'https://image.shutterstock.com/image-photo/peach-isolated-on-white-background-600w-610892250.jpg',
+    'https://image.shutterstock.com/image-photo/mixed-fruit-basket-isolated-on-600w-610892251.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +65,7 @@ class _ProductListScreenState extends State<ProductListScreen> {
           Expanded(
             child: ListView.builder(
               itemCount: productName.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return Card(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -67,39 +74,51 @@ class _ProductListScreenState extends State<ProductListScreen> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
-                             mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
                           children: [
                             Image(
                               height: 100,
                               width: 100,
-                              image: NetworkImage(productImage[index].toString()),
+                              image: NetworkImage(
+                                productImage[index].toString(),
                               ),
-                            Text(productName[index].toString(),
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
                             ),
+                             SizedBox(width: 10),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  productName[index].toString(),
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  '${productUnit[index]} ${productPrice[index]}',
+                                 
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(productUnit[index].toString(),
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            ),
+
                             Spacer(),
                           ],
-                        )
+                        ),
                       ],
                     ),
                   ),
-
                 );
-              } ),),
+              },
+            ),
+          ),
         ],
       ),
     );
