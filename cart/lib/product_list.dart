40027858b376575934ff9ddb59dd8e9086c813cc
Salman Khan <salmantechhub336@gmail.com>
2026-04-13 23:45:41 +0500

    done

--- cart/lib/product_list.dart
+++ cart/lib/product_list.dart
@@ -16,7 +16,7 @@ class ProductListScreen extends StatefulWidget {
 
 class _ProductListScreenState extends State<ProductListScreen> {
   DBHelper dbHelper = DBHelper();
-  int cartCount = 0;
+  // int cartCount = 0;
 
   List<String> productName = [
     'Mango',
@@ -151,9 +151,9 @@ class _ProductListScreenState extends State<ProductListScreen> {
                                     print(error.toString());
                                   });
 
-                              setState(() {
-                                cartCount = cart.getCounter();
-                              });
+                              // setState(() {
+                              //   cartCount = cart.getCounter();
+                              // });
 
                               ScaffoldMessenger.of(context).showSnackBar(
                                 SnackBar(
