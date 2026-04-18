a6a99613c0adf05c23a8cf0a400ad90e07f4f71a
Salman Khan <salmantechhub336@gmail.com>
2026-04-19 01:24:25 +0500

    app bar for the cart screen

--- cart/lib/cart_screen.dart
+++ cart/lib/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cart/cart_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as badges;
 import 'package:provider/provider.dart';
 
 class CartScreen extends StatefulWidget {
@@ -13,6 +14,36 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<CartProvider>(context);
-    return const Placeholder();
+    return Scaffold(
+       appBar: AppBar(
+        title: const Text(
+          'Product List',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
+        actions: [
+          Center(
+            child: badges.Badge(
+              label: Consumer<CartProvider>(
+                builder: (context, value, child){
+                  return Text(
+                    value.getCounter().toString(),
+                  // cartCount.toString(),
+                  style: const TextStyle(color: Colors.white, fontSize: 13),
+                );
+                },
+              ),
+              child: const Icon(Icons.shopping_bag_outlined),
+            ),
+          ),
+          const SizedBox(width: 20),
+        ],
+      ),
+    );
   }
 }
